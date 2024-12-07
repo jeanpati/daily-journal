97309adef6c8e0dc9c98e93f52391d485f80ffc2
Jean Patimeteeporn <jpatimet@gmail.com>
2024-12-07 02:59:52 -0600

    added basic styling

--- scripts/main.js
+++ scripts/main.js
@@ -2,19 +2,19 @@ const contentTarget = document.querySelector(".main");
 
 const renderHTML = () => {
   contentTarget.innerHTML = `<section class="main-content"><form class="entryForm">
-      <fieldset>
+      <fieldset class="formField">
         <label for="entryDate">Date</label>
         <input type="date" name="entryDate" class="entryForm__date"></input>
       </fieldset>
-      <fieldset>
+      <fieldset class="formField">
         <label for="entryTitle">Title</label>
         <input type="text" name="entryTitle" class="entryForm__title"></input>
       </fieldset>
-      <fieldset>
+      <fieldset class="formField">
         <label for="entryBody">Body</label>
         <textarea name="entryBody" class="entryForm__body"></textarea>
       </fieldset>
-      <fieldset>
+      <fieldset class="formField">
         <label for="entryMood">Mood</label>
         <select name="entryMood" class="entryForm__mood">
         <option>good</option>
