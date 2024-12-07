7c9492b28d9a8d1581d479287369e06fd41691ac
Jean Patimeteeporn <jpatimet@gmail.com>
2024-12-07 04:16:27 -0600

    added json-server to project

--- scripts/main.js
+++ scripts/main.js
@@ -3,11 +3,13 @@ import { form } from "./form.js";
 
 const contentTarget = document.querySelector(".main");
 
+const entriesHTML = await entryList();
+
 const renderHTML = () => {
   contentTarget.innerHTML = `<section class="main-content">
     <article id="form">${form()}</article>
     
-    <article id="entries">${entryList()}</article>
+    <article id="entries">${entriesHTML}</article>
     </section>`;
 };
 
