70cf13fe9fe7dbbb97addd5049ee225a267984f9
Jean Patimeteeporn <jpatimet@gmail.com>
2024-12-07 02:38:06 -0600

    set up basic html and main module

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,33 @@
-const contentTarget = document.querySelector("#content");
+const contentTarget = document.querySelector(".main");
 
-const renderHTML = () => {};
+const renderHTML = () => {
+  contentTarget.innerHTML = `<section class="main-content"><form class="entryForm">
+      <fieldset>
+        <label for="entryDate">Date</label>
+        <input type="date" name="entryDate" class="entryForm__date"></input>
+      </fieldset>
+      <fieldset>
+        <label for="entryTitle">Title</label>
+        <input type="text" name="entryTitle" class="entryForm__title"></input>
+      </fieldset>
+      <fieldset>
+        <label for="entryBody">Body</label>
+        <textarea name="entryBody" class="entryForm__body"></textarea>
+      </fieldset>
+      <fieldset>
+        <label for="entryMood">Mood</label>
+        <select name="entryMood" class="entryForm__mood">
+        <option>good</option>
+        <option>ok</option>
+        <option>bad</option>
+        </select>
+      </fieldset>
+      <button>Submit</button>
+    </form>
+
+    
+    <article id="entries"></article>
+    </section>`;
+};
+
+renderHTML();
