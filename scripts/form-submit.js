975df169d7c85abd41a0ca88c4b4b97a5b9d802f
Jean Patimeteeporn <jpatimet@gmail.com>
2024-12-07 21:02:02 -0600

    added transient state module and added feature to submit and save form data to database

---
+++ scripts/form-submit.js
@@ -0,0 +1,59 @@
+import {
+  setBody,
+  setDate,
+  setMood,
+  setTitle,
+  postEntry,
+} from "./transient-state.js";
+
+const handleDateChoice = (changeEvent) => {
+  if (changeEvent.target.name === "entryDate") {
+    setDate(changeEvent.target.value);
+    console.log(changeEvent.target.value);
+  }
+};
+const handleTitleChoice = (changeEvent) => {
+  if (changeEvent.target.name === "entryTitle") {
+    setTitle(changeEvent.target.value);
+    console.log(changeEvent.target.value);
+  }
+};
+const handleBodyChoice = (changeEvent) => {
+  if (changeEvent.target.name === "entryBody") {
+    setBody(changeEvent.target.value);
+    console.log(changeEvent.target.value);
+  }
+};
+const handleMoodChoice = (changeEvent) => {
+  if (changeEvent.target.name === "entryMood") {
+    setMood(changeEvent.target.value);
+    console.log(changeEvent.target.value);
+  }
+};
+
+export const addInputListeners = () => {
+  document
+    .querySelector("#entryDate")
+    .addEventListener("change", handleDateChoice);
+  document
+    .querySelector("#entryTitle")
+    .addEventListener("input", handleTitleChoice);
+  document
+    .querySelector("#entryBody")
+    .addEventListener("input", handleBodyChoice);
+  document
+    .querySelector("#entryMood")
+    .addEventListener("change", handleMoodChoice);
+};
+
+const handleSubmitClick = (clickEvent) => {
+  if (clickEvent.target.id === "submitEntry") {
+    postEntry();
+  }
+};
+
+export const addSubmitListener = () => {
+  document
+    .querySelector("#submitEntry")
+    .addEventListener("click", handleSubmitClick);
+};
--- scripts/form.js
+++ scripts/form.js
@@ -1,27 +1,28 @@
-let formHTML = "";
 export const form = () => {
+  let formHTML = "";
   formHTML += `<form class="entryForm">
       <fieldset class="formField">
         <label for="entryDate">Date</label>
-        <input type="date" name="entryDate" class="entryForm__date"></input>
+        <input type="date" name="entryDate" id="entryDate" class="entryForm__date"></input>
       </fieldset>
       <fieldset class="formField">
         <label for="entryTitle">Title</label>
-        <input type="text" name="entryTitle" class="entryForm__title"></input>
+        <input type="text" name="entryTitle" placeholder="Title" id="entryTitle" class="entryForm__title"></input>
       </fieldset>
       <fieldset class="formField">
         <label for="entryBody">Body</label>
-        <textarea name="entryBody" class="entryForm__body"></textarea>
+        <textarea name="entryBody" id="entryBody" placeholder="Type your journal entry here" class="entryForm__body"></textarea>
       </fieldset>
       <fieldset class="formField">
         <label for="entryMood">Mood</label>
-        <select name="entryMood" class="entryForm__mood">
+        <select name="entryMood" id="entryMood" class="entryForm__mood">
+        <option value="" disabled selected>Select your mood</option>
         <option>good</option>
         <option>ok</option>
         <option>bad</option>
         </select>
       </fieldset>
-      <button>Submit</button>
+      <button type="button" id="submitEntry" class="submitEntry">Submit</button>
     </form>`;
   return formHTML;
 };
